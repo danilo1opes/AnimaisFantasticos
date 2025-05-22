3ba1af3d061b7a55071e625c1ffb5c633dfbe599
Danilo Lopes <contatolopesdan@gmail.com>
2025-05-22 12:23:26 -0300

    ⚡️feat: improve icon with react and add function toogleClick for dl

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,5 +1,17 @@
+import { useState } from 'react';
+import { FaChevronDown } from 'react-icons/fa';
 import { questionsData } from '../Data/Questions';
 export function Faq() {
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+  function handleClick(index: number) {
+    if (activeIndex === index) {
+      setActiveIndex(null);
+    } else {
+      setActiveIndex(index);
+    }
+  }
+
   return (
     <section className="grid grid-cols-1 md:grid-cols-1 xl:grid-cols-1fr gap-6 px-6 md:px-10 mb-16">
       <div className="col-span-1 md:col-span-2">
@@ -11,10 +23,22 @@ export function Faq() {
         <div>
           {questionsData.map((item, index) => (
             <dl className="mb-6 md:mt-8" key={index}>
-              <dt className=" flex justify-between items-center faq-question text-lg text-brand-redSix hover:underline cursor-pointer">
+              <dt
+                onClick={() => handleClick(index)}
+                className="flex justify-between items-center faq-question text-lg text-brand-redSix hover:underline cursor-pointer"
+              >
                 {item.pergunta}
+                <FaChevronDown
+                  className={`ml-2 transition-transform duration-300 ${
+                    activeIndex === index ? 'rotate-180' : 'rotate-0'
+                  }`}
+                />
               </dt>
-              <dd className="text-start text-brand-secondary">
+              <dd
+                className={`${
+                  activeIndex === index ? 'block' : 'hidden'
+                } text-start text-brand-secondary mt-2`}
+              >
                 {item.resposta}
               </dd>
             </dl>
