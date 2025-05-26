eecda1a45d45a71dad6a82ad1d3d4783b0adaa79
Danilo Lopes <contatolopesdan@gmail.com>
2025-05-26 10:58:15 -0300

    ⚡️feat: "Implementation of animated numbers"

--- src/Data/NumbersData.ts
+++ src/Data/NumbersData.ts
@@ -4,21 +4,21 @@ export const numbersData: StatCard[] = [
   {
     id: 1,
     title: 'Lobos',
-    value: '4,890',
+    value: 4890,
   },
   {
     id: 2,
     title: 'Raposas',
-    value: '5,000',
+    value: 5000,
   },
   {
     id: 3,
     title: 'Esquilos',
-    value: '12,401',
+    value: 12401,
   },
   {
     id: 4,
     title: 'Ursos',
-    value: '223',
+    value: 223,
   },
 ];
